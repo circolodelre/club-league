2b94f684d417f1a550a157950fa5934e952fa7ca
Francesco Bianco <info.francescobianco@gmail.com>
2017-11-23 16:44:53 +0100

    Update artefact

--- skeleton.sh
+++ skeleton.sh
@@ -3,10 +3,26 @@
 ARTEFACT_REPO=https://github.com/circolodelre/club-league
 SKELETON_REPO=https://github.com/javanile/slim-skeleton
 
+## Update artefact before sync skeleton
 git remote set-url origin $ARTEFACT_REPO
-
 git pull
 git add .
 git add *
 git commit -m "Update artefact"
+git push
+
+## Update skeleton
+git remote set-url origin $SKELETON_REPO
+git pull
+git add .
+git add *
+git commit -m "Update skeleton"
+git push
+
+## Refresh artefact after skeleton sync
+git remote set-url origin $ARTEFACT_REPO
+git pull
+git add .
+git add *
+git commit -m "Refresh artefact"
 git push
