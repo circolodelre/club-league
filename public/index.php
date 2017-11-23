1c923b848a6347213aa32db21aca2abfc84fc1f6
Francesco Bianco <info.francescobianco@gmail.com>
2017-11-23 18:34:52 +0100

    Update artefact

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
@@ -12,16 +13,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
-// Instantiate the app
-$settings = require __DIR__ . '/../app/settings.php';
+$app = new \Slim\App();
 
-$app = new \Slim\App($settings);
-
-// Set up dependencies
-require __DIR__ . '/../app/dependencies.php';
-// Register middleware
-require __DIR__ . '/../app/middleware.php';
-// Register routes
+require __DIR__ . '/../app/services.php';
 require __DIR__ . '/../app/routes.php';
-// Run!
+
 $app->run();
