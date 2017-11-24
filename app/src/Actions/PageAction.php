c5879ab4f7a50219d53e3512104823e33a055786
Francesco Bianco <info.francescobianco@gmail.com>
2017-11-24 01:14:38 +0100

    Initial commit

--- app/src/Actions/PageAction.php
+++ app/src/Actions/PageAction.php
@@ -8,20 +8,12 @@ use Slim\Http\Response;
 
 class PageAction
 {
-    private $view;
-    private $logger;
+    protected $view;
+    protected $logger;
 
     public function __construct(Twig $view, LoggerInterface $logger)
     {
         $this->view = $view;
         $this->logger = $logger;
-    }
-
-    public function __invoke(Request $request, Response $response, $args)
-    {
-        $this->logger->info("Home page action dispatched");
-
-        $this->view->render($response, 'home.twig');
-        return $response;
     }
 }
